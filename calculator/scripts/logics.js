e2e93f95ee0e487e73c80872ecc853fa39b3ee23
AlehYurchuk <alehyurchuk@gmail.com>
2023-02-19 19:08:14 +0100

    llll

--- calculator/scripts/logics.js
+++ calculator/scripts/logics.js
@@ -7,15 +7,16 @@ const digit = ['0','1','2','3','4','5','6','7','8','9','.'];
 const action = ['-','+','X','/'];
 
 
-
+const out = document.querySelector('.cals_scr p')
 
 function clearAll (){
     a = '';
     b = '';
     sign = '';
     finish = false;
-    elem0.textContent = 0;
+    out.textContent = 0;
 }
+
 
 document.querySelector('.ac').addEventListener = clearAll;
 
@@ -26,10 +27,17 @@ document.querySelector('.buttons').addEventListener = (event) =>{
     if(event.target.classList.contains('ac')) return;
     elem0.textContent = '';
 
+    out.textContent = '';
     const key = event.target.textContent;
 
     if(digit.includes(key)){
         a += key;
+        console.log(a, b, sign);
+        elem0.textContent = a ;
+    }
+    if(action.includes(key)){
+        sign += key;
+        console.log(a, b, sign);
         elem0.textContent = a ;
     }
 };
